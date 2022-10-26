a3af21c265bbbbc1a5c6d546aa83658523e370ba
Admin <Admin@OMEGA.local>
2022-10-26 23:07:44 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Product from './Product';
+ const Main = (props) => {
+    const {products, onAdd, onRemove, cartItems} =props;
+  return (
+    <div className=' block col-2'>
+        <h2>Products</h2>
+        <div className='row'>
+           { products.map((product)=>(
+            <Product  key={product.id}
+            item={cartItems.find((x)=> x.id === product.id)}
+            product={product} onAdd={onAdd} onRemove={onRemove}/>
+           ))}
+        </div>
+    </div>
+  )
+}
+export default Main;
